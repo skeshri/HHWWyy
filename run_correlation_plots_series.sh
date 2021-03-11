be690d23cb2630bb4236a59e30efd21fa3991955
Joshuha <jthomasw@cern.ch>
2021-03-11 12:12:15 +0100

    correlation heat maps

---
+++ run_correlation_plots_series.sh
@@ -0,0 +1,3 @@
+time python input_DNN_corr_heatmap.py -p  DYJetsToLL_M-50 GJet_Pt-20to40 TTGJets TTGG ttHJetToGG VBFHToGG GluGluHToGG VHToGG
+
+#HHWWgg DiPhoton_80plus GJet_Pt-40toInf QCD_Pt-40toInf
